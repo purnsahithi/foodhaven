fd7f4442f8020d4b9a96a30d883ba5d18f4052cb
purnsahithi <purnasahithimuppidi@gmail.com>
2022-05-16 23:28:11 +0530

    firstcommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { JoinusComponent } from './joinus/joinus.component';
+import { PaymentComponent } from './payment/payment.component';
+import { ShopComponent } from './shop/shop.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'joinus',component:JoinusComponent},
+  {path:'payment',component:PaymentComponent},
+  {path:'shop',component:ShopComponent},
+  {path:'feedback',component:FeedbackComponent},
+  {path:'contactus',component:ContactusComponent},
+  {path:'',redirectTo:'/home',pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { JoinusComponent } from './joinus/joinus.component';
+import { PaymentComponent } from './payment/payment.component';
+import { ShopComponent } from './shop/shop.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    JoinusComponent,
+    PaymentComponent,
+    ShopComponent,
+    FeedbackComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
